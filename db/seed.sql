54b1160de909a04507926708ca207b37a398c3a2
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-10-24 21:58:53 -0400

    sets up database, schema and seed sql files

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album ) VALUES
+('Thunderstruck', 'AC/DC', 'The Razors Edge'),
+('One', 'Creed','My Own Prison'),
+('Take it to the Limit', 'Eagles', 'One of These Nights');
