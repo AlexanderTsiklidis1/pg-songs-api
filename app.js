414bd843e0498fd2ade98d2d1ec757c7ae822102
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-10-24 21:48:49 -0400

    preliminarily builds database

---
+++ app.js
@@ -0,0 +1,23 @@
+const cors = require("cors");
+const express = require("express");
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Our Songs App");
+});
+
+const songsController = require("./controllers/songsController.js");
+app.use("/songs", songsController);
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found");
+  });
+
+module.exports = app;
