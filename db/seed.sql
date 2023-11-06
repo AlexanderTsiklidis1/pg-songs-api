c82c3cd628278ebd49df6b2ea9dde59e7f8889fd
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-05 19:43:59 -0500

    final changes before render, for now

--- db/seed.sql
+++ db/seed.sql
@@ -1,27 +1,28 @@
 
 \c songs_dev;
 
-INSERT INTO songs (name, artist, album, time, is_favorite)
+INSERT INTO songs (id, name, artist, album, time, is_favorite)
 VALUES
-  ('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', '5:55', true),
-  ('Stairway to Heaven', 'Led Zeppelin', 'Untitled IV', '8:03', false),
-  ('Hotel California', 'Eagles', 'Hotel California', '6:30', true),
-  ('Paint It Black', 'The Rolling Stones', 'Aftermath', '3:22', false),
-  ('Comfortably Numb', 'Pink Floyd', 'The Wall', '6:23', true),
-  ('Sweet Child O'' Mine', 'Guns N'' Roses', 'Appetite for Destruction', '5:55', false),
-  ('Light My Fire', 'The Doors', 'The Doors', '7:08', true),
-  ('Purple Haze', 'Jimi Hendrix', 'Are You Experienced', '2:50', false),
-  ('Smoke on the Water', 'Deep Purple', 'Machine Head', '5:40', true),
-  ('Black Dog', 'Led Zeppelin', 'Led Zeppelin IV', '4:55', false);
+  ('1','Bohemian Rhapsody', 'Queen', 'A Night at the Opera', '5:55', true),
+  ('2','Stairway to Heaven', 'Led Zeppelin', 'Untitled IV', '8:03', false),
+  ('3','Hotel California', 'Eagles', 'Hotel California', '6:30', true),
+  ('4','Paint It Black', 'The Rolling Stones', 'Aftermath', '3:22', false),
+  ('5','Comfortably Numb', 'Pink Floyd', 'The Wall', '6:23', true),
+  ('6','Sweet Child O'' Mine', 'Guns N'' Roses', 'Appetite for Destruction', '5:55', false),
+  ('7','Light My Fire', 'The Doors', 'The Doors', '7:08', true),
+  ('8','Purple Haze', 'Jimi Hendrix', 'Are You Experienced', '2:50', false),
+  ('9','Smoke on the Water', 'Deep Purple', 'Machine Head', '5:40', true),
+  ('10','Black Dog', 'Led Zeppelin', 'Led Zeppelin IV', '4:55', false);
 
-  INSERT INTO artists (name, genre)
+INSERT INTO artists (id, name, genre)
 VALUES
-  ('Queen', 'Rock'),
-  ('Led Zeppelin', 'Rock'),
-  ('Eagles', 'Rock'),
-  ('The Rolling Stones', 'Rock'),
-  ('Pink Floyd', 'Rock'),
-  ('Guns N'' Roses', 'Rock'),
-  ('The Doors', 'Rock'),
-  ('Jimi Hendrix', 'Rock'),
-  ('Deep Purple', 'Rock');
+  ('1','Queen', 'Rock'),
+  ('2','Led Zeppelin', 'Rock'),
+  ('3','Eagles', 'Rock'),
+  ('4','The Rolling Stones', 'Rock'),
+  ('5','Pink Floyd', 'Rock'),
+  ('6','Guns N'' Roses', 'Rock'),
+  ('7','The Doors', 'Rock'),
+  ('8','Jimi Hendrix', 'Rock'),
+  ('9','Deep Purple', 'Rock');
+  
